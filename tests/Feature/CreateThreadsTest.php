44d4269276096e5c28e63f9f4a94b8eb7950e80a
Pixlforge <admin@pixlforge.ch>
2017-06-11 08:04:12 +0200

    Lesson 28

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -87,7 +88,10 @@ class CreateThreadsTest extends TestCase
         $response->assertStatus(204);
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
+
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+
+        $this->assertEquals(0, Activity::count());
     }
 
     /**
