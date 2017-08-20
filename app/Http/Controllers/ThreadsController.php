b387c1b815fc42bb7acda8fbb4080c8f212392de
Pixlforge <celien@pixlforge.ch>
2017-08-20 09:45:41 +0200

    summary
    description

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Channel;
 use App\Filters\ThreadFilters;
 use App\Thread;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -78,6 +79,9 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
+        $key = sprintf("users.%s.visits.%s", auth()->id(), $thread->id);
+        cache()->forever($key, Carbon::now());
+
         return view('threads.show', compact('thread'));
     }
 
