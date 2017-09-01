fbeeb6346fa7e461d8f099a630c292770713c9ee
Pixlforge <celien@pixlforge.ch>
2017-09-01 07:11:25 +0200

    Lesson 74

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -9,14 +9,14 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        } catch (\Exception $exception) {
+        $user = User::where('confirmation_token', request('token'))->first();
+
+        if (! $user) {
             return redirect(route('threads'))
                 ->with('flash', 'Unknown token');
         }
+
+        $user->confirm();
 
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed! You may now post to the forum.');
